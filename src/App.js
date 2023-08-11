be2a2e5c532fcc124555da09982e63037a432667
Ash45# <ashishsahoo2015@gmail.com>
2023-08-11 12:04:39 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { generateRandomQuote } from "./generateRandomQuote";
+import Card from "./Card";
 
 const App = () => {
   const [quote, setQuote] = useState();
@@ -25,24 +26,8 @@ const App = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="mb-4">
-        <h1 className="text-3xl font-bold mb-2">Random Quote Generator</h1>
-        <div className="border-t border-b w-20 mx-auto"></div>
-      </div>
-      {quote ? (
-        <div className="text-center">
-          <p className="text-xl font-semibold">{quote.quote}</p>
-          <p className="italic text-gray-600 mt-2">- {quote.author}</p>
-        </div>
-      ) : null}
-
-      <button
-        className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        onClick={generateNewQuote}
-      >
-        New Quote
-      </button>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-300 to-purple-400">
+      <Card quote={quote} generateNewQuote={generateNewQuote} />
     </div>
   );
 };
